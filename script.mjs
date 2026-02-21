d9922633be87b5ca4164ed2f770747ca772cea70
aliciawt <aliciawynonatjahjadi@gmail.com>
2026-02-21 14:15:51 +0100

    Add user dropdown list

--- script.mjs
+++ script.mjs
@@ -1,11 +1,15 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
+import { getUserIDs } from "./data.mjs";
 
-import { countUsers } from "./common.mjs";
+// grabbing DOM elements
+const userDropdown = document.getElementById("user-dropdown");
 
-window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
-};
+// populating user dropdown
+const userIDs = getUserIDs();
+
+userIDs.forEach(id => {
+  const option = document.createElement("option");
+  option.value = id;
+  option.textContent = `User ${id}`;
+
+  userDropdown.appendChild(option);
+});
