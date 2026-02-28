4665689d1e0b8c783b297e01055352b015933b4c
aliciawt <aliciawynonatjahjadi@gmail.com>
2026-02-28 15:31:13 +0100

    Add top listens by length

--- script.mjs
+++ script.mjs
@@ -6,7 +6,7 @@ const mostListenedSongByNumber = document.getElementById("most-listened-song")
 const mostListenedArtistByNumber = document.getElementById("most-listened-artist")
 const mostListenedSongByNumberFriday = document.getElementById("most-listened-song-Friday")
 const mostListenedSongByLength = document.getElementById("most-listened-song-v2")
-const mostListenedArtistByLength = document.getElementById("most-listened-length-v2")
+const mostListenedArtistByLength = document.getElementById("most-listened-artist-v2")
 const mostListenedSongByLengthFriday = document.getElementById("most-listened-song-Friday-v2")
 const mostListenedSongInARow = document.getElementById("most-times-in-a-row")
 const songListenedToEveryday = document.getElementById("listen-everyday")
@@ -37,75 +37,131 @@ let activeUser = userDropdown.value;
 
 userDropdown.addEventListener("change", (e) => {
   activeUser = e.target.value;
+  console.clear();
   countSongTimes();
   countArtistTimes();
   countSongTimesFridayNight();
 })
 
-// function to count NUMBER of songs
+// function to determine top song by times listened
 function countSongTimes () {
   const listenEvents = getListenEvents(activeUser);
   let count = {};
+  let duration = {};
   listenEvents.forEach(event => {
     const songID = event.song_id;
+    const songDuration = getSong(songID).duration_seconds;
     count[songID] = (count[songID] || 0) + 1;
+    duration[songID] = count[songID] * songDuration;
   })
 
   console.log(count);
+  console.log(duration);
 
-  let maxCount = 0;
-  let topSong = null;
+  let maxCountTimes = 0;
+  let maxCountDuration = 0;
+  let maxCountDurationMinutes = 0;
+  let topSongTimes = null;
+  let topSongDuration = null;
 
   for (let id in count) {
-    if (count[id] > maxCount) {
-      maxCount = count[id];
-      topSong = id;
+    if (count[id] > maxCountTimes) {
+      maxCountTimes = count[id];
+      topSongTimes = id;
     }
   }
 
-  if (topSong !== null) {
+  for (let id in duration) {
+    if (duration[id] > maxCountDuration) {
+      maxCountDuration = duration[id];
+      topSongDuration = id;
+    }
+  }
+
+  maxCountDurationMinutes = Math.floor(maxCountDuration / 60);
+
+  if (topSongTimes !== null) {
     mostListenedSongByNumber.innerHTML =
-    `The song you listened to the most is <strong>${topSong}</strong>.`;
+    `The song you listened to the most is <strong>${topSongTimes}</strong>. 
+    You listened to this song <strong>${maxCountTimes} times</strong>.`;
   } else {
     mostListenedSongByNumber.innerHTML =
-    "You don't have a top song.";
+    "No data available to display your top song.";
+  }
+
+  if (topSongDuration !== null) {
+    mostListenedSongByLength.innerHTML =
+    `The song you listened to the longest is <strong>${topSongDuration}</strong>. 
+    You listened to this song for <strong>more than ${maxCountDurationMinutes} minutes</strong>!
+    Are you not sick of it?`;
+  } else {
+    mostListenedSongByLength.innerHTML =
+    "No data available to display your top song.";
   }
 }
 
-// function to count NUMBER of artist
+// function to determine top artist by times listened
 function countArtistTimes () {
   const listenEvents = getListenEvents(activeUser);
   let count = {};
+  let duration = {};
   listenEvents.forEach(event => {
     const songID = event.song_id;
+    const songDuration = getSong(songID).duration_seconds;
     const artist = getSong(songID).artist;
     count[artist] = (count[artist] || 0) + 1; 
+    duration[artist] = (count[artist]) * songDuration;
   })
 
   console.log(count);
+  console.log(duration);
 
-  let maxCount = 0;
-  let topArtist = null;
+  let maxCountTimes = 0;
+  let maxCountDuration = 0;
+  let maxCountDurationMinutes = 0;
+  let topArtistTimes = null;
+  let topArtistDuration = null;
 
   for (let id in count) {
-    if (count[id] > maxCount) {
-      maxCount = count[id];
-      topArtist = id;
+    if (count[id] > maxCountTimes) {
+      maxCountTimes = count[id];
+      topArtistTimes = id;
     }
   }
+
+  for (let id in duration) {
+    if (duration[id] > maxCountDuration) {
+      maxCountDuration = duration[id];
+      topArtistDuration = id;
+    }
+  }
+
+  maxCountDurationMinutes = Math.floor(maxCountDuration / 60);
   
-  if (topArtist !== null) {
+  if (topArtistTimes !== null) {
     mostListenedArtistByNumber.innerHTML =
-    `The artist you listened to the most is <strong>${topArtist}</strong>.`;
+    `The artist you listened to the most is <strong>${topArtistTimes}</strong>. 
+    You listened to this artist <strong>${maxCountTimes} times</strong>.`;
   } else {mostListenedArtistByNumber.innerHTML = 
-    "You don't have a top artist.";
+    "No data available to display your top artist.";
+  }
+
+  if (topArtistDuration !== null) {
+    mostListenedArtistByLength.innerHTML =
+    `The artist you listened to the longest is <strong>${topArtistDuration}</strong>. 
+    You listened to this artist for <strong>more than ${maxCountDurationMinutes} minutes</strong>!
+    Obsessed much?`;
+  } else {
+    mostListenedArtistByLength.innerHTML =
+    "No data available to display your top artist.";
   }
 }
 
-// function to count NUMBER of song on Friday night
+// function to determine top song on Friday night by times listened
 function countSongTimesFridayNight () {
   const listenEvents = getListenEvents(activeUser);
   let count = {};
+  let duration = {};
   listenEvents.forEach(event => {
     const songID = event.song_id;
     const time = event.timestamp;
@@ -113,34 +169,62 @@ function countSongTimesFridayNight () {
     const hour = date.getHours();
     const dayName = date.toString().slice(0,3);
     if (dayName === "Fri" && hour >= 17 || dayName === "Sat" && hour < 4) {
+      const songDuration = getSong(songID).duration_seconds;
       count[songID] = (count[songID] || 0) + 1;
+      duration[songID] = count[songID] * songDuration;
     };
   })
 
   console.log(count);
+  console.log(duration);
 
-  let maxCount = 0;
-  let topSongFriday = null;
+  let maxCountTimes = 0;
+  let maxCountDuration = 0;
+  let maxCountDurationMinutes = 0;
+  let topSongTimesFriday = null;
+  let topSongDurationFriday = null;
 
   for (let id in count) {
-    if (count[id] > maxCount) {
-      maxCount = count[id];
-      topSongFriday = id;
+    if (count[id] > maxCountTimes) {
+      maxCountTimes = count[id];
+      topSongTimesFriday = id;
     }
   }
+
+  for (let id in duration) {
+    if (duration[id] > maxCountDuration) {
+      maxCountDuration = duration[id];
+      topSongDurationFriday = id;
+    }
+  }
+
+  maxCountDurationMinutes = Math.floor(maxCountDuration / 60);
   
-  if (topSongFriday !== null) {
+  if (topSongTimesFriday !== null) {
     mostListenedSongByNumberFriday.innerHTML =
-    `The song you listened to the most is on Friday nights (between 5pm and 4am) <strong>${topSongFriday}</strong>.`;
+    `The song you listened to the most on Friday nights (between 5pm and 4am) is <strong>${topSongTimesFriday}</strong>.
+    You listened to this song for <strong>${maxCountTimes} times</strong>.
+    You okay baby?`;
   } else {
     mostListenedSongByNumberFriday.innerHTML = 
-    "You don't listen to any songs on Friday nights (between 5pm and 4am).";
+    "No data available to display your top song on Friday nights (between 5pm and 4am).";
+  }
+
+  if (topSongDurationFriday !== null) {
+    mostListenedSongByLengthFriday.innerHTML =
+    `The song you listened to the most on Friday nights (between 5pm and 4am) is <strong>${topSongDurationFriday}</strong>.
+    You listened to this song for more than <strong>${maxCountDurationMinutes} minutes</strong>!
+    You okay baby?`;
+  } else {
+    mostListenedSongByLengthFriday.innerHTML = 
+    "No data available to display your top song on Friday nights (between 5pm and 4am).";
   }
 }
 
+// function to determine top song by listening time
+
 // can the functions be combined somehow...? don't forget to add the friday
 // will it be efficient because they are supposed to be inputted in 2 different lines anyway? --> store values in different variables
-// edge test for null (no songs listened to in case of user 4) --> return 'you   have no listening history'
+// edge test for null (no songs listened to in case of user 4) --> return 'you have no listening history'
 // make date, hour, day name global?
-// attach results to DOM
-// you dont have a top song/top artist? technically incorrect, should do 'no listening history' instead on user 4
+// separate functions: grab object data, determining top, and attaching to DOM?
